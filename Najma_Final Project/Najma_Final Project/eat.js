f6f12425bdf18bb967fb51525179ecac58e90716
Najma Dawood-McCarthy <Najmad@users.noreply.github.com>
2016-12-19 01:35:51 -0500

    Delete eat.js

--- Najma_Final Project/Najma_Final Project/eat.js
+++
@@ -1,9 +0,0 @@
- function eat(){
-   if(c == control.x && a[i] == control.y){
-    control.bigger();
-//   } else if (foods[i].goodbad == 1){
-    
-   }
-//   foods[i].updatePosition();
-//   foods[].drawfood();
- }
